d2e0afa5d1194fefca731a705614953125f25fac
jgutierrez746 <jose.gutierrez.roa@gmail.com>
2025-11-08 20:50:20 -0300

    Se realiza ajusto de formato en modelo de creacion de tabla temáticas

--- modelos/modelos.go
+++ modelos/modelos.go
@@ -9,9 +9,9 @@ import (
 type TematicasModel struct {
 	bun.BaseModel `bun:"table:tematicas"`
 
-	ID        int64     `bun:",pk,autoincrement" json:"id"`
+	ID        int64     `bun:",pk,autoincrement" json:"id,omitempty"`
 	Nombre    string    `bun:"nombre,notnull" json:"nombre" binding:"required"`
-	Slug      string    `bun:"slug,notnull,unique" json:"slug" binding:"required"`
-	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	Slug      string    `bun:"slug,notnull,unique" json:"slug"`
+	CreatedAt time.Time `bun:",nullzero,type:timestamp,notnull,default:current_timestamp" json:"created_at,omitempty"`
+	UpdatedAt time.Time `bun:",nullzero,type:timestamp,notnull,default:current_timestamp,on_update:current_timestamp" json:"updated_at,omitempty"`
 }
